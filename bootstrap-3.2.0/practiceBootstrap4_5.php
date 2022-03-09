fd822b674b2a7580ac2489766ae3fc4ece465f6b
123Ilya123 <izraylov.ilya@mail.ru>
2022-03-09 23:51:15 +0300

    09.03

---
+++ bootstrap-3.2.0/practiceBootstrap4_5.php
@@ -0,0 +1,14 @@
+<?php 
+    require "PAPKA/footear.php";
+?>
+
+<div class="container">
+<img src="img/dog.jpg" alt="..." class="img-rounded"> 
+        <img src="img/dog.jpg" alt="..." class="img-circle"> 
+        <img src="img/dog.jpg" alt="..." class="img-thumbnail">
+</div>
+
+
+<?php 
+    require "PAPKA/header.php";
+?>
